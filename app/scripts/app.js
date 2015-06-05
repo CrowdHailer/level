248b1fbd41569393112d8b6d8014ad69803dc97d
Peter Saxton <peterhsaxton@gmail.com>
2015-06-05 08:45:50 +0100

    setup on device motion

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,8 +2,8 @@
 
 var app = require('ampersand-app');
 var Router = require('./router');
-var Main = require('./models/main')
-var MainView = require('./views/main_view')
+var Main = require('./models/main');
+var MainView = require('./views/main_view');
 
 module.exports = app.extend({
     init: function(){
@@ -22,8 +22,11 @@ module.exports = app.extend({
                 el: document.body,
                 model: app.model
             });
-            window.requestAnimationFrame(setModelOrienation)
+            // window.requestAnimationFrame(setModelOrienation);
             app.router.history.start();
+            if (window.DeviceMotionEvent) {
+                window.addEventListener("devicemotion", deviceMotionHandler);
+            }
             app.ready();
         });
     },
@@ -35,116 +38,44 @@ module.exports = app.extend({
 
 module.exports.init();
 
-var degtorad = Math.PI / 180; // Degree-to-Radian conversion
-
-function getBaseRotationMatrix( alpha, beta, gamma ) {
-	var _x = beta  ? beta  * degtorad : 0; // beta value
-	var _y = gamma ? gamma * degtorad : 0; // gamma value
-	var _z = alpha ? alpha * degtorad : 0; // alpha value
-
-	var cX = Math.cos( _x );
-	var cY = Math.cos( _y );
-	var cZ = Math.cos( _z );
-	var sX = Math.sin( _x );
-	var sY = Math.sin( _y );
-	var sZ = Math.sin( _z );
-
-	//
-	// ZXY-ordered rotation matrix construction.
-	//
-
-	var m11 = cZ * cY - sZ * sX * sY;
-	var m12 = - cX * sZ;
-	var m13 = cY * sZ * sX + cZ * sY;
-
-	var m21 = cY * sZ + cZ * sX * sY;
-	var m22 = cZ * cX;
-	var m23 = sZ * sY - cZ * cY * sX;
-
-	var m31 = - cX * sY;
-	var m32 = sX;
-	var m33 = cX * cY;
-
-	return [
-		m11,    m12,    m13,
-		m21,    m22,    m23,
-		m31,    m32,    m33
-	];
+function dotProduct(v1, v2) {
+    return v1.x * v2.x + v1.y * v2.y + v1.z * v2.z;
 }
 
-function getScreenTransformationMatrix( screenOrientation ) {
-	var orientationAngle = screenOrientation ? screenOrientation * degtorad : 0;
-
-	var cA = Math.cos( orientationAngle );
-	var sA = Math.sin( orientationAngle );
-
-	// Construct our screen transformation matrix
-	var r_s = [
-		cA,    -sA,    0,
-		sA,    cA,     0,
-		0,     0,      1
-	];
-
-	return r_s;
+function magnitude(v) {
+    return Math.sqrt(v.x * v.x + v.y * v.y + v.z * v.z);
 }
 
-function matrixMultiply( a, b ) {
-	var final = [];
-
-	final[0] = a[0] * b[0] + a[1] * b[3] + a[2] * b[6];
-	final[1] = a[0] * b[1] + a[1] * b[4] + a[2] * b[7];
-	final[2] = a[0] * b[2] + a[1] * b[5] + a[2] * b[8];
-
-	final[3] = a[3] * b[0] + a[4] * b[3] + a[5] * b[6];
-	final[4] = a[3] * b[1] + a[4] * b[4] + a[5] * b[7];
-	final[5] = a[3] * b[2] + a[4] * b[5] + a[5] * b[8];
-
-	final[6] = a[6] * b[0] + a[7] * b[3] + a[8] * b[6];
-	final[7] = a[6] * b[1] + a[7] * b[4] + a[8] * b[7];
-	final[8] = a[6] * b[2] + a[7] * b[5] + a[8] * b[8];
-
-	return final;
+function scale(a, v) {
+    // a: scalar multiplier
+    // v: 3 dimensional vector
+    return {x: a * v.x, y: a * v.y, z: a * v.z};
 }
 
-var deviceOrientationData, currentScreenOrientation;
-currentScreenOrientation = 0
+var down = {x: 0, y: 0, z: -1};
+
+var currentScreenOrientation = 0;
 
 window.addEventListener('orientationchange', function() {
-	currentScreenOrientation = window.orientation;
+       currentScreenOrientation = window.orientation;
 }, false);
 
-var deviceOrientationData = {};
 
-window.addEventListener('deviceorientation', function( event ) {
-	deviceOrientationData = event;
-}, false);
-
-function computeMatrix() {
-	var rotationMatrix = getBaseRotationMatrix(
-		deviceOrientationData.alpha,
-		deviceOrientationData.beta,
-		deviceOrientationData.gamma
-	); // R
-
-	var screenTransform = getScreenTransformationMatrix( currentScreenOrientation ); // r_s
-
-	var screenAdjustedMatrix = matrixMultiply( rotationMatrix, screenTransform ); // R_s
-
-	// var worldTransform = getWorldTransformationMatrix(); // r_w
-    //
-	// var finalMatrix = matrixMultiply( screenAdjustedMatrix, worldTransform ); // R_w
-
-	return screenAdjustedMatrix; // [ m11, m12, m13, m21, m22, m23, m31, m32, m33 ]
-}
-
-function setModelOrienation() {
-  var m = computeMatrix()
-  var z = - 1 * m[8]
-  var y = - 1 * m[5]
-  var x = - 1 * m[2]
-  var angleX = Math.atan(x/z)
-  var angleY = Math.atan(y/z)
-  window.level.angleX = angleX;
-  window.level.angleY = angleY;
-  window.requestAnimationFrame(setModelOrienation)
+function deviceMotionHandler(deviceMotionEvent) {
+    var vector = deviceMotionEvent.accelerationIncludingGravity;
+    var cosTheta = dotProduct(down, vector) / magnitude(vector);
+    var thetaRad = Math.acos(cosTheta);
+    var theta = 180 * thetaRad / Math.PI;
+    var orientation;
+    if (currentScreenOrientation == 90) {
+        orientation = {x: -1 * vector.y, y: vector.x, z: 0};
+    } else if (currentScreenOrientation == -90) {
+        orientation = {x: vector.y, y: -1 * vector.x, z: 0};
+    } else {
+        orientation = {x: vector.x, y: vector.y, z: 0};
+    }
+    var angleFactor = theta / magnitude(orientation);
+    var position = scale(angleFactor, orientation);
+    module.exports.model.angleX = -1 * position.x;
+    module.exports.model.angleY = position.y;
 }
