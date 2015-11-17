85fb99f237d1fc1495653d62fd565c70ffe98a7e
CrowdHailer <peterhsaxton@gmail.com>
2015-11-17 11:09:59 +0000

    very basic flux setup

--- app/scripts/boot.js
+++ app/scripts/boot.js
@@ -1,11 +1,48 @@
 /*jshint esnext: true */
 "use strict";
 
-function App() {
-  window.console.log("Booting....");
-  return {};
+var ACTIONS = {
+  ACCELEROMETER_READING: "ACCELEROMETER_READING"
+};
+function Store(argument) {
+  var neutral = {x: 0, y: 0, z: 0};
+  var down = {x: 0, y: 0, z: 0};
+
+  return Object.create({
+    dispatch: function dispatch(action) {
+      switch (action.type) {
+        case ACTIONS.ACCELEROMETER_READING:
+            window.console.log("update down");
+            window.console.log(action.vector);
+
+          break;
+        default:
+
+      }
+    }
+  }, {
+    x: {
+      get: function () {
+        return down.x;
+      }
+    }
+  });
 }
 
-var app = App();
+var store = Store();
+
+function Actions() {
+  window.console.log("Booting....");
+  return {
+    accelerometerReading: function (vector) {
+      store.dispatch({
+        type: ACTIONS.ACCELEROMETER_READING,
+        vector: vector
+      });
+    }
+  };
+}
+
+var app = Actions();
 
 export default app;
