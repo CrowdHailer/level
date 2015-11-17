c0130f585f9d67cd26bf98495cdabb67a7e74612
CrowdHailer <peterhsaxton@gmail.com>
2015-11-17 15:20:05 +0000

    create dispatcher

--- app/scripts/boot.js
+++ app/scripts/boot.js
@@ -5,31 +5,53 @@ import * as ACTIONS from "./actions";
 import OrientationStore from "./orientation-store";
 import SplashComponent from "./splash-component";
 
-
 var store = OrientationStore();
 
-function Actions() {
+function SplashStore() {
+  return Object.create({
+    dispatch: function () {
+      console.log("splash store dispatching");
+    }
+  });
+}
+var splashStore = SplashStore();
+
+var DispatcherPrototype = {
+  dispatch: function (action) {
+    this.stores.forEach(function (store) {
+      store.dispatch(action);
+    });
+  }
+};
+function Dispatcher(stores) {
+  return Object.create(DispatcherPrototype, {
+    stores: {
+      get: function () { return stores; }
+    }
+  });
+}
+
+var dispatcher = Dispatcher([store, splashStore]);
+
+function Actions(dispatcher) {
   window.console.log("Booting....");
   return {
     accelerometerReading: function (vector) {
-      store.dispatch({
+      dispatcher.dispatch({
         type: ACTIONS.ACCELEROMETER_READING,
         vector: vector
       });
     },
     acknowledgeSplash: function (vector) {
-      store.dispatch({
+      dispatcher.dispatch({
         type: ACTIONS.ACKNOWLEDGE_SPLASH,
         vector: vector
       });
-    },
-    stores: [
-      store
-    ]
+    }
   };
 }
 
-var app = Actions();
+var app = Actions(dispatcher);
 
 var $splashComponent = window.document.querySelector("[data-component~=splash-component]");
 var splashComponent = SplashComponent($splashComponent, app);
