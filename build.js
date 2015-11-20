59b1dc5711d05cba21af643609fb2434124a572b
CrowdHailer <peterhsaxton@gmail.com>
2015-11-20 12:30:02 +0000

    setup build and run

---
+++ build.js
@@ -0,0 +1,19 @@
+"use strict";
+
+var rollup = require('rollup');
+var typescript = require('rollup-plugin-typescript');
+
+rollup.rollup({
+  entry: 'app/scripts/boot.ts',
+  plugins: [
+    typescript({
+      sourceMap: true
+    })
+  ]
+}).then(function (bundle) {
+  bundle.write({
+    format: "iife",
+    moduleName: "level",
+    dest: "www/app.js"
+  });
+});
