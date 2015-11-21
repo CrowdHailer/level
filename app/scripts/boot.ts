2c5773cfc909161c59285552d65950d3a110d1f8
CrowdHailer <peterhsaxton@gmail.com>
2015-11-21 10:32:46 +0000

    clean boot file

--- app/scripts/boot.ts
+++ app/scripts/boot.ts
@@ -1,29 +1,28 @@
 console.log("starting");
 /// <reference path="./gator.d.ts" />
-import * as Vector from "./vector.ts";
-import * as Action from "./actions.ts";
 
-import SpiritLevelState from "./spirit_level_state.ts";
+// Setup the stores
 import SpiritLevelStore from "./spirit_level_store.ts";
-
-
-
-var $spiritLevel = document.querySelector("[data-component~=spirit-level]");
-
 var spiritLevel = new SpiritLevelStore();
-spiritLevel.callbacks.push(function a (state){ console.log(state); });
 
-
-
-import UserInput from "./user_input.ts";
-import SpiritLevelDisplay from "./spirit_level_display.ts";
-var spiritLevelDisplay = SpiritLevelDisplay($spiritLevel);
-
+// Requires dom
 import { ready } from "./dom.ts";
 
+import SpiritLevelDisplay from "./spirit_level_display.ts";
+
+import UserInput from "./user_input.ts";
+
 ready(function(){
+  // Create all the presenters once dom elements are available
+  var $spiritLevel = document.querySelector("[data-component~=spirit-level]");
+  var spiritLevelDisplay = SpiritLevelDisplay($spiritLevel);
+
+
+  // register the presenters as watchers to individual stores
+  // DEBT calls in to array. make add presenter
   spiritLevel.callbacks.push(spiritLevelDisplay.update);
-  // spiritLevel.dispatch({type: "ACCELEROMETER_READING", payload: Vector.create({x: 5, y: 11})});
+
+  // Capture input Events
   var ui = new UserInput(document, spiritLevel);
 });
 
