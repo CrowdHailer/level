7356301990fcb91ef1479b31aeb2d0e1692c7497
CrowdHailer <peterhsaxton@gmail.com>
2015-12-14 20:54:50 +0000

    separate delegator

--- client/boot.js
+++ client/boot.js
@@ -3,14 +3,4 @@
 
 window.console.log("hello");
 
-import { closest, isDescendant } from "./utils/dom";
-
-export function on(eventName, selector, callback){
-  document.addEventListener(eventName, function(evt){
-    var $trigger = closest(selector, evt.target);
-    if ($trigger && isDescendant(document, $trigger)) {
-      evt.trigger = $trigger;
-      return callback(evt);
-    }
-  });
-}
+export * from "./utils/dom";
--- client/utils/dom.js
+++ client/utils/dom.js
@@ -21,3 +21,15 @@ export function isDescendant(parent, child) {
   }
   return false;
 }
+
+export function Delegator(root){
+  this.on = function(eventName, selector, callback){
+    root.addEventListener(eventName, function(evt){
+      var $trigger = closest(selector, evt.target);
+      if ($trigger && isDescendant(root, $trigger)) {
+        evt.trigger = $trigger;
+        return callback(evt);
+      }
+    });
+  };
+}
