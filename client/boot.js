329614e9d835477c22abb62ae2397f060e0322de
CrowdHailer <peterhsaxton@gmail.com>
2015-12-16 15:53:33 +0000

    fix single level state

--- client/boot.js
+++ client/boot.js
@@ -3,7 +3,6 @@
 
 import Map from "carbide/map";
 import View from "./view";
-import $ from "anon/dom";
 
 import State from "./state";
 
@@ -12,16 +11,6 @@ function App(){
   var dispatcher = Dispatcher.create(window.console);
   var state = State.create(window.location);
 
-  Object.defineProperty(this, "menuOpen", {
-    get: function(){
-      return state.menuOpen;
-    }
-  });
-  Object.defineProperty(this, "theme", {
-    get: function(){
-      return state.theme;
-    }
-  });
   Object.defineProperty(this, "state", {
     get: function(){
       return state;
@@ -32,12 +21,12 @@ function App(){
     dispatcher = dispatcher.register(cb);
   };
   this.openMenu = function(){
-    if (state.menu.open) { return; }
+    if (state.menuOpen) { return; }
     state = State.openMenu(state);
     dispatcher.dispatch();
   };
   this.closeMenu = function(){
-    if (!state.menu.open) { return; }
+    if (!state.menuOpen) { return; }
     state = State.closeMenu(state);
     dispatcher.dispatch();
   };
@@ -56,7 +45,7 @@ function App(){
 }
 
 import * as QString from "query-string";
-function Location(projection){
+function Location(app){
   function url(projection){
     var path = "/";
     if (projection.menuOpen) {
@@ -70,9 +59,9 @@ function Location(projection){
   }
 
 
-  history.replaceState({}, "", url(projection));
+  history.replaceState({}, "", url(app.state));
   function update(){
-    history.pushState({}, "", url(projection));
+    history.pushState({}, "", url(app.state));
   }
   this.update = update;
 }
@@ -98,14 +87,14 @@ var controller = Controller(document);
 
 var app = new App();
 var $view = new View(document);
-$view.isSpiritLevelMinimised = app.menuOpen;
-$view.theme = app.theme;
+$view.isSpiritLevelMinimised = app.state.menuOpen;
+$view.theme = app.state.theme;
 var myLocation = new Location(app);
 app.onUpdate(myLocation.update);
 myLocation.update();
 app.onUpdate(function(){
-  $view.isSpiritLevelMinimised = app.menuOpen;
-  $view.theme = app.theme;
+  $view.isSpiritLevelMinimised = app.state.menuOpen;
+  $view.theme = app.state.theme;
   console.log(app.state.accelerometerReading.x)
   $view.angleX = app.state.accelerometerReading.x.toFixed(2) || "0";
 });
