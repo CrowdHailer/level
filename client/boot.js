6750f95fda1794d0e8fa4778019b48348462b3be
CrowdHailer <peterhsaxton@gmail.com>
2015-12-16 10:56:56 +0000

    read query string

--- client/boot.js
+++ client/boot.js
@@ -4,6 +4,39 @@
 import Map from "../node_modules/carbide/map";
 import View from "./view";
 
+import * as QString from "query-string";
+function Client(){
+  var $view = new View(document);
+  window.$view = $view;
+  var queryString = location.search;
+  var query = QString.parse(queryString);
 
-var $view = new View(document);
-window.$view = $view;
+  this.state = Map({
+    menu: Map({
+      open: false,
+    }),
+    accelerometer: Map({
+      reading: Map({
+        x: 0,
+        y: 0,
+        z: 0
+      }),
+    }),
+    theme: query.theme || "APPLE"
+  });
+  this.location = new Location();
+  this.location.update(this.state);
+}
+
+function Location(){
+  function update(state){
+    var queryString = QString.stringify({
+      theme: state.theme
+    });
+    console.log(queryString);
+    history.replaceState({url: queryString}, "", "?" + queryString);
+  }
+  this.update = update;
+}
+
+new Client();
--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,6 @@
 /* jshint esnext: true*/
+import commonjs from 'rollup-plugin-commonjs';
+import npm from 'rollup-plugin-npm';
 
 export default {
   entry: 'client/boot.js',
@@ -6,5 +8,8 @@ export default {
   moduleName: "Level",
   dest: "www/client.js",
   sourceMap: true,
-
+  plugins: [
+    npm({ jsnext: true, main: true }),
+    commonjs()
+  ]
 };
