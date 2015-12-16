0fb680f45ab42b95bfc2382c4ca0edc3fe835430
CrowdHailer <peterhsaxton@gmail.com>
2015-12-16 11:06:01 +0000

    type error on view

--- client/view.js
+++ client/view.js
@@ -3,15 +3,27 @@
 
 export default function View($root){
   var $bubble = $root.querySelector("[data-display~=bubble]");
+  var $angleX = $root.querySelector("[data-display~=angleX]");
+  var $angleY = $root.querySelector("[data-display~=angleY]");
 
   Object.defineProperty(this, "angleX", {
     set: function(angle){
+      if (typeof angle !== 'string') {
+        throw new TypeError("angleX property should be a string");
+      }
+
       $bubble.setAttribute("cx", angle);
+      $angleX.innerHTML = angle;
     }
   });
   Object.defineProperty(this, "angleY", {
     set: function(angle){
+      if (typeof angle !== 'string') {
+        throw new TypeError("angleY property should be a string");
+      }
+      
       $bubble.setAttribute("cy", angle);
+      $angleY.innerHTML = angle;
     }
   });
 }
