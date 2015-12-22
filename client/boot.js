c7a2bcd5b3104e444e2c71b4a17a7e1c0c2f9b60
CrowdHailer <peterhsaxton@gmail.com>
2015-12-22 10:59:00 +0000

    parse uri on startup

--- client/boot.js
+++ client/boot.js
@@ -4,6 +4,13 @@
 import App from "./app";
 import Component from "./component";
 import Router from "./router";
+import * as URI from "./uri";
+console.log("hello");
+var uri = URI.parseLocation(window.location);
+console.log(uri)
+
+// start by parsing url
+// app state has theme, log-level, acceleration, menu-active
 
 var app = new App();
 var router = new Router(app);
