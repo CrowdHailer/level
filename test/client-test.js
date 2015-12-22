2ce2f3633fa73e9997901df3b1f7f1f35f3fa662
CrowdHailer <peterhsaxton@gmail.com>
2015-12-22 09:23:59 +0000

    setup tests

--- test/client-test.js
+++ test/client-test.js
@@ -2,8 +2,6 @@
 /* jshint jasmine: true */
 "use strict";
 
-import Client from "../client/client";
-
 describe("The Level client", function(){
   it("should have a passing test", function(){
     expect(true).toBe(true);
