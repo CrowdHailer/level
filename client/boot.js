d8f5c61edf2d83d88926a70c4d994615efef4df3
CrowdHailer <peterhsaxton@gmail.com>
2016-01-03 15:18:22 +0000

    implement all usecases

--- client/boot.js
+++ client/boot.js
@@ -2,7 +2,13 @@ import Level from "./level";
 import * as Logger from "./anon/logger";
 
 var logger = window.console;
-var level = Level();
+var level = Level({setup: "RUNNING"});
 level.logger = logger;
+
+level.view = {
+  render: function(projection){
+    console.log(projection);
+  }
+};
 
 export default level;
--- client/level.js
+++ client/level.js
@@ -3,26 +3,54 @@ export default function Level(state){
 
   var level = this;
   this.setupComplete = function(){
+    if (state.setup === "COMPLETED") { return; }
+
+    state = Object.assign({}, state, {setup: "COMPLETED"});
+
+    level.view.render(state);
     level.logger.info("Setup Complete");
   };
 
-  this.closeSplashScreen = function(){
+  this.acknowledgeSplashScreen = function(){
+    if (state.splashScreenAcknowledged === true) { return; }
+
+    state = Object.assign({}, state, {splashScreenAcknowledged: true});
+
+    level.view.render(state);
     level.logger.info("Close Splash Screen");
   };
 
   this.showMenu = function(){
+    if (state.menuVisible === true) { return; }
+
+    state = Object.assign({}, state, {menuVisible: true});
+
+    level.view.render(state);
     level.logger.info("Show Menu");
   };
 
   this.hideMenu = function(){
+    if (state.menuVisible === false) { return; }
+
+    state = Object.assign({}, state, {menuVisible: false});
+
+    level.view.render(state);
     level.logger.info("Hide Menu");
   };
 
-  this.selectTheme = function(){
+  this.selectTheme = function(theme){
+    if (state.theme === theme) { return; }
+
+    state = Object.assign({}, state, {theme: theme});
+
+    level.view.render(state);
     level.logger.info("Select Theme");
   };
 
-  this.newReading = function(){
+  this.newReading = function(reading){
+    state = Object.assign({}, state, {reading: reading});
+
+    level.view.render(state);
     level.logger.info("New Reading");
   };
 }
