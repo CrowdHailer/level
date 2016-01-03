144bf13dbb6deba8f646c5b327ec9afe0f31522b
CrowdHailer <peterhsaxton@gmail.com>
2016-01-03 17:31:39 +0000

    menu action on document

--- client/view.js
+++ client/view.js
@@ -13,5 +13,22 @@ function url(projection){
 export default function View() {
   this.render = function(projection){
     history.pushState({}, "", url(projection));
+    var minimised = projection.menuVisible;
+    if ($spiritLevel) {
+      if (minimised) {
+        $spiritLevel.classList.add("minimised");
+      } else {
+        $spiritLevel.classList.remove("minimised");
+      }
+    }
   };
 }
+
+import { ready } from "./anon/dom";
+
+var $root, $spiritLevel;
+ready(function(){
+  $root = document.documentElement;
+  $spiritLevel = $root.querySelector("[data-display~=spirit-level]");
+  console.log($spiritLevel);
+});
