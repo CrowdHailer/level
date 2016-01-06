ffa8025982b00e0684b45817d50741dcc4f14c20
CrowdHailer <peterhsaxton@gmail.com>
2016-01-06 12:16:13 +0000

    clean branch

--- test/client-test.js
+++
@@ -1,9 +0,0 @@
-/* jshint esnext: true */
-/* jshint jasmine: true */
-"use strict";
-
-describe("The Level client", function(){
-  it("should have a passing test", function(){
-    expect(true).toBe(true);
-  });
-});
--- test/other-test.js
+++
@@ -1,5 +0,0 @@
-describe("The Level client", function(){
-  it("should have a passing test", function(){
-    expect(true).toBe(true);
-  });
-});
